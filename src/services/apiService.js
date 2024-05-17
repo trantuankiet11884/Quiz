2382d147b3633b175f8f4ee389cf94242aa800e1
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-05-17 08:55:53 +0700

    api services and customize axios

---
+++ src/services/apiService.js
@@ -0,0 +1,13 @@
+import instance from "../utils/axios";
+
+const postCreateNewUser = (email, password, username, role, image) => {
+  const data = new FormData();
+  data.append("email", email);
+  data.append("password", password);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+  return instance.post("api/v1/participant", data);
+};
+
+export { postCreateNewUser };
