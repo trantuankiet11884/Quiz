c3467b1e5e7aee1440831b9ecc7072a0b5764087
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-05-20 11:11:04 +0700

    create fn back home in cpn sidebar admin

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -16,8 +16,12 @@ import {
   FaTachometerAlt,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const AdminSideBar = (props) => {
   const { image, collapsed, toggle, handleToggleSidebar } = props;
+  const navigate = useNavigate();
+
   return (
     <>
       <ProSidebar
@@ -66,7 +70,11 @@ const AdminSideBar = (props) => {
               className="sidebar-btn-wrapper"
               style={{ padding: "20px 24px" }}
             >
-              <a className="sidebar-btn" target="_blank">
+              <a
+                className="sidebar-btn"
+                target="_blank"
+                onClick={() => navigate("/")}
+              >
                 <FaArrowAltCircleLeft />
                 &nbsp;
                 <span
