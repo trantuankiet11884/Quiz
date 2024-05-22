d4a29d6662f7e5323faf97ab92a1d5937a0ac2c2
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-05-22 08:36:20 +0700

    redux-persist save info user localstorage

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Layout } from "./Layout";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "nprogress/nprogress.css";
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor} loading={null}>
         <Layout />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
