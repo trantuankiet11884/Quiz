4bb6ab8b77b395bbf8191460582e1b51faeaf116
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-05-22 10:04:38 +0700

    design quiz layout

--- src/components/User/DetailsQuiz.js
+++ src/components/User/DetailsQuiz.js
@@ -1,11 +1,13 @@
+import "./detailquiz.scss";
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import _ from "lodash";
 const DetailsQuiz = (props) => {
+  const location = useLocation();
   const { id } = useParams();
   const quizId = id;
-
+  console.log(location);
   useEffect(() => {
     fetchQuestions();
   }, [id]);
@@ -31,7 +33,32 @@ const DetailsQuiz = (props) => {
         .value();
     }
   };
-  return <div className="detail-quiz-container">DetailsQuiz</div>;
+  return (
+    <div className="detail-quiz-container container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId}: {location?.state?.quizTitle}
+        </div>
+        <hr />
+        <div className="q-body">
+          <img src="" alt="" />
+        </div>
+        <div className="q-content">
+          <div className="question">Question 1: what should you do?</div>
+          <div className="answer">
+            <div className="a-child">A. 1</div>
+            <div className="a-child">B. 1</div>
+            <div className="a-child">C. 1</div>
+          </div>
+        </div>
+        <div className="footer">
+          <button className="btn btn-default">Prev</button>
+          <button className="btn btn-secondary">Next</button>
+        </div>
+      </div>
+      <div className="right-content">count down</div>
+    </div>
+  );
 };
 
 export default DetailsQuiz;
--- src/components/User/ListQuiz.js
+++ src/components/User/ListQuiz.js
@@ -35,7 +35,11 @@ const ListQuiz = (props) => {
                   <p className="card-text">{quiz.description}</p>
                   <button
                     className="btn btn-primary"
-                    onClick={() => navigate(`/quiz/${quiz.id}`)}
+                    onClick={() =>
+                      navigate(`/quiz/${quiz.id}`, {
+                        state: { quizTitle: quiz.description },
+                      })
+                    }
                   >
                     Go somewhere
                   </button>
