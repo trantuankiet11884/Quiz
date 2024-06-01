77d4eb0a0340dcc97c84d18e62d6c7d7db2da5d8
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-06-01 08:24:17 +0700

    design dashboard and update something ... file scss

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 import AdminSideBar from "./SideBar";
 import "./admin.scss";
 import { FaBars } from "react-icons/fa";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { Outlet } from "react-router-dom";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import Languages from "../Header/Languages";
 const Admin = (props) => {
   const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="admin-container">
       <div className="admin-sidebar">
@@ -13,11 +16,20 @@ const Admin = (props) => {
       </div>
       <div className="admin-content">
         <div className="admin-header">
-          <FaBars
-            size={24}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{ cursor: "pointer" }}
-          />
+          <span onClick={() => setCollapsed(!collapsed)}>
+            <FaBars
+              size={24}
+              style={{ cursor: "pointer" }}
+              className="leftside"
+            />
+          </span>
+          <div className="rightside">
+            <NavDropdown title="Settings" id="basic-nav-dropdown">
+              <NavDropdown.Item>Logout</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+            </NavDropdown>
+            {/* <Languages /> */}
+          </div>
         </div>
         <div className="admin-main">
           <PerfectScrollbar>
--- src/components/Admin/Content/Dashboard.js
+++ src/components/Admin/Content/Dashboard.js
@@ -1,5 +1,31 @@
+import "./dashboard.scss";
+import {
+  BarChart,
+  Line,
+  LineChart,
+  CartesianGrid,
+  YAxis,
+  XAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 const Dashboard = (props) => {
-  return <div></div>;
+  return (
+    <div className="dashboard-container">
+      <div className="title">Dashboard</div>
+      <div className="content">
+        <div className="content-left">
+          <div className="child">Total Users</div>
+          <div className="child">Total Quizzes</div>
+          <div className="child">Total Questions</div>
+          <div className="child">Answers</div>
+        </div>
+        <div className="content-right">
+          <BarChart></BarChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
